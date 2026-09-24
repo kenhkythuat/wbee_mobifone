250541d45299d9b8417bebc7968ff10eba74eec7
kenhkythuat <thuanphat7.embedded@gmail.com>
2026-09-24 09:25:13 +0700

    version 3.3

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -13,7 +13,7 @@
 
 // Codename of the farm, where we deploy this node to.
 
-#define VERSION_WBEE "3.2"
+#define VERSION_WBEE "3.3"
 
 #define OTA_ENABLE 1
 #define OTA_MANIFEST_URL "https://raw.githubusercontent.com/kenhkythuat/wbee_mobifone/feature/ota_mobifone/ota/manifest.json"
